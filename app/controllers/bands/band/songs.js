1c0f4cc95c0fd06d68d2bf37488b313b1aae37c9
Brian Westerweel <brian.westerweel@fabriquartz.com>
2016-07-04 15:16:37 +0200

    Finished chapter 12

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,8 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: {
+    sortBy: 'sort',
+    searchTerm: 's',
+  },
   title: '',
   songCreationStarted: false,
+  sortBy: 'ratingDesc',
+  sortProperties: Ember.computed('sortBy', function() {
+    var options = {
+      'ratingDesc': 'rating:desc,title:asc',
+      'ratingAsc':  'rating:asc,title:asc',
+      'titleDesc':  'title:desc',
+      'titleAsc':   'title:asc',
+    };
+    return options[this.get('sortBy')].split(',');
+  }),
+
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
+
+  searchTerm: '',
+
+  matchingSongs: Ember.computed('model.songs.@each.title',
+  'searchTerm', function() {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    return this.get('model.songs').filter(function(song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+     });
+  }),
 
   isAddButtonDisabled: Ember.computed('title', function() {
     return Ember.isEmpty(this.get('title'));
@@ -26,6 +52,6 @@ export default Ember.Controller.extend({
 
     enableSongCreation: function() {
       this.set('songCreationStarted', true);
-    }
+    },
   }
 });
